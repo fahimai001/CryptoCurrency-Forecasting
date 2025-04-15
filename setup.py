e169457c383c779b24c2a4af9701c3befa682ec7
Fahim Sabir <fahimsabircstec001@gmail.com>
2025-04-15 05:29:56 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project to Forecast Close Price for both Bitcoin and Ethereum',
+    author='fahimX',
+    license='',
+)
